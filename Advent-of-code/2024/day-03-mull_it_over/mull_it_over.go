d44e46bd3bba0a8ec8ac80774eadb6c9119b9119
mashingan <rahmat.d.ruffy@gmail.com>
2024-12-08 05:38:53 +0700

    add day 3 part 2

--- Advent-of-code/2024/day-03-mull_it_over/mull_it_over.go
+++ Advent-of-code/2024/day-03-mull_it_over/mull_it_over.go
@@ -10,8 +10,12 @@ import (
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	const rfmt = `mul\(\d{1,3},\d{1,3}\)`
-	// f, err := os.Open("sample.txt")
+	const (
+		rfmt = `mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`
+		// f, err := os.Open("sample.txt")
+		do   = "do()"
+		dont = "don't()"
+	)
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -20,17 +24,25 @@ func main() {
 	mulexpr := regexp.MustCompile(rfmt)
 	scanner := bufio.NewScanner(f)
 	sum := 0
+	enabled := true
 	for scanner.Scan() {
 		txt := scanner.Text()
 		// log.Println(txt)
 		var l, r int
 		for _, s := range mulexpr.FindAllString(txt, -1) {
 			log.Println(s)
+			if s == do {
+				enabled = true
+				continue
+			} else if s == dont {
+				enabled = false
+				continue
+			}
 			if n, err := fmt.Sscanf(s, "mul(%d,%d)", &l, &r); err != nil {
 				log.Println(err)
 			} else if n != 2 {
 				log.Println("scanned not match, expected 2 got:", n)
-			} else {
+			} else if enabled {
 				sum += l * r
 			}
 		}
