dc744714434e4fee71b166892483e37e0fc9d27c
mashingan <rahmat.d.ruffy@gmail.com>
2024-12-08 23:30:28 +0700

    add day 5 part 2

--- Advent-of-code/2024/day-05-print_queue/print_queue.go
+++ Advent-of-code/2024/day-05-print_queue/print_queue.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -23,6 +24,7 @@ func main() {
 	updates := [][]int{}
 	for scanner.Scan() {
 		txt := scanner.Text()
+		// log.Println(txt)
 		if txt == "" {
 			pageOrderingDone = true
 			continue
@@ -50,31 +52,31 @@ func main() {
 		}
 		updates = append(updates, ints)
 	}
+	// log.Println(updates)
+	// log.Println(pageOrder)
 	summid := 0
+	summUpdate := 0
 	for _, up := range updates {
-		validUpdate := true
-		for i := 0; i < len(up); i++ {
-			order := up[i]
-			before := up[:i]
-			mustAfter := []int{}
-			if i+1 < len(up) {
-				mustAfter = up[i+1:]
-			}
-			orders := pageOrder[order]
-			ordering := intersection(orders, mustAfter)
-			mustNotBefore := intersection(before, orders)
-			if len(ordering) != len(mustAfter) || len(mustNotBefore) > 0 {
-				validUpdate = false
-			}
-		}
+		// log.Println("====updates:", up, "====")
+		validUpdate, correctedUpdate := checkOrdering(up, pageOrder)
 		if validUpdate {
 			mid := up[len(up)/2]
 			summid += mid
-
+		} else {
+			// log.Println("==incorrect update:", up, "==")
+			// log.Println("==correction update:", correctedUpdate, "==")
+			invalidOrdering := !validUpdate
+			for invalidOrdering {
+				validUpdate, correctedUpdate = checkOrdering(correctedUpdate, pageOrder)
+				invalidOrdering = !validUpdate
+			}
+			summUpdate += correctedUpdate[len(correctedUpdate)/2]
 		}
+		// log.Println("====end updates====")
 
 	}
 	fmt.Println(summid)
+	fmt.Println(summUpdate)
 }
 
 func in[T comparable](e T, arr []T) bool {
@@ -94,4 +96,41 @@ func intersection[T comparable](a, b []T) []T {
 		}
 	}
 	return result
+}
+
+func checkOrdering(up []int, pageOrder map[int][]int) (bool, []int) {
+	validUpdate := true
+	updateCorrections := slices.Clone(up)
+	updateOrderPos := map[int]int{}
+	for i, n := range up {
+		updateOrderPos[n] = i
+	}
+	for i := 0; i < len(up); i++ {
+		order := up[i]
+		// log.Println("--order:", order, "--")
+		before := up[:i]
+		mustAfter := []int{}
+		if i+1 < len(up) {
+			mustAfter = up[i+1:]
+		}
+		orders := pageOrder[order]
+		ordering := intersection(orders, mustAfter)
+		mustNotBefore := intersection(before, orders)
+		if len(ordering) != len(mustAfter) || len(mustNotBefore) > 0 {
+			validUpdate = false
+			for _, n := range mustNotBefore {
+				pos := updateOrderPos[n]
+				// log.Println("i:", i, ", pos:", pos)
+				updateCorrections[i], updateCorrections[pos] = updateCorrections[pos], updateCorrections[i]
+				// log.Println("current correction:", updateCorrections)
+			}
+		}
+		// log.Println("order:", order)
+		// log.Println("mustNotBefore:", before)
+		// log.Println("mustAfter:", mustAfter)
+		// log.Println("ordering:", ordering)
+		// log.Println("orders:", orders)
+		// log.Println("mustNotBefore:", mustNotBefore)
+	}
+	return validUpdate, updateCorrections
 }
