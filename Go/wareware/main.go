d2d72e29d2c036178019f70a40da48b197e9130f
mashingan <rahmat.d.ruffy@gmail.com>
2026-08-10 20:16:30 +0700

    add simple operations example

--- Go/wareware/main.go
+++ Go/wareware/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"maps"
+	"math/rand"
+	"strings"
 	"time"
 
 	"github.com/oklog/ulid/v2"
@@ -24,21 +27,23 @@ func NewWarehouse(name string) *Warehouse {
 }
 
 func NewWarehouseInventory(name string, inv map[string]Inventory) *Warehouse {
-	return &Warehouse{
+	w := &Warehouse{
 		Id:          ulid.Make().String(),
 		Name:        name,
 		inventories: inv,
 		capacities:  map[string]uint64{},
 	}
+	return w
 }
 
 func NewWarehouseCapacities(name string, caps map[string]uint64) *Warehouse {
-	return &Warehouse{
+	w := &Warehouse{
 		Id:          ulid.Make().String(),
 		Name:        name,
 		inventories: map[string]Inventory{},
 		capacities:  caps,
 	}
+	return w
 }
 
 func (w *Warehouse) AddInventory(itemName string, inv Inventory) {
@@ -76,6 +81,14 @@ func (w *Warehouse) TakeInventories(list map[string]uint64) map[string]error {
 	return errs
 }
 
+func (w *Warehouse) Cap(unit string) (uint64, error) {
+	cp, ok := w.capacities[unit]
+	if !ok {
+		return 0, fmt.Errorf("not supported capacities: [%s]", unit)
+	}
+	return cp, nil
+}
+
 func (w *Warehouse) AddCapacities(caps map[string]uint64) {
 	maps.Copy(w.capacities, caps)
 }
@@ -85,9 +98,31 @@ type Inventory struct {
 	Qty uint64
 }
 
+func (i Inventory) String() string {
+	return fmt.Sprintf("%s\nQty: %d", i.Item, i.Qty)
+}
+
 type Item struct {
 	Id, Name, Unit string
 	Infos          map[string]Fields
+}
+
+func (i Item) String() string {
+	info := fmt.Sprintf(`Id: %s
+Name: %s,
+Unit: %s,
+==========
+Additional Tags:
+`, i.Id, i.Name, i.Unit)
+	bld := &strings.Builder{}
+	bld.WriteString(info)
+	for k, v := range i.Infos {
+		fmt.Fprintf(bld, "%s: type: %s, default-value: %s, filename(optional): %s\n",
+			k, v.FieldType, v.Value, v.filename)
+
+	}
+	info += "=========="
+	return bld.String()
 }
 
 type FieldType uint8
@@ -99,6 +134,13 @@ const (
 	FieldDocument
 	FieldImage
 )
+
+func (f FieldType) String() string {
+	fieldsInfo := [...]string{
+		"Alphanumeric", "Datetime", "Numeric", "Document", "Image",
+	}
+	return fieldsInfo[f]
+}
 
 type Fields struct {
 	FieldType
@@ -126,5 +168,58 @@ type Activity struct {
 	datetime      time.Time
 }
 
+func (a Activity) String() string {
+	bld := &strings.Builder{}
+	itemsflow := "items in"
+	if a.flowitem == ItemOut {
+		itemsflow = "items out"
+	}
+
+	bld.WriteString("======start-activity========\n")
+	fmt.Fprintf(bld, "At time %s, there's %s with list:\n",
+		a.datetime.Format("Monday 15:04:05+07 02-Jan-2006"),
+		itemsflow)
+	for _, inv := range a.inventories {
+		fmt.Fprint(bld, inv)
+	}
+	bld.WriteString("\n========end-activity========")
+	return bld.String()
+}
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	w := NewWarehouseCapacities("test-gudang", map[string]uint64{
+		"pcs": 5_000,
+	})
+	item := Item{
+		Id:   ulid.Make().String(),
+		Name: "onderdil",
+		Unit: "pcs",
+		Infos: map[string]Fields{
+			"part-code":       {FieldAlpha, "part-code", "ab333L", ""},
+			"production-year": {FieldNumeric, "production-year", "2020", ""},
+			"gen":             {FieldAlpha, "gen", "1Q2", ""},
+		},
+	}
+	target, _ := w.Cap("pcs")
+	sentInv := uint64(0)
+	daysMax := 60
+	for sentInv < target {
+		toSent := max(rand.Intn(int(target-sentInv+1)), 0)
+		log.Println("toSent:", toSent)
+		if toSent == 0 {
+			break
+		}
+		inv := Inventory{item, uint64(toSent)}
+		sentInv += uint64(toSent)
+		tosub := max(rand.Intn(daysMax), daysMax/2)
+		daysMax -= tosub
+		thedate := time.Now().Add(24 * time.Hour * -1 * time.Duration(tosub))
+		act := Activity{ItemIn, []Inventory{inv}, ulid.Make().String(), w.Id, thedate}
+		w.AddInventory(item.Id, inv)
+		log.Println("activity:\n", act)
+	}
+	fmt.Printf("%#v\n", *w)
+	fmt.Println(w)
+
 }
